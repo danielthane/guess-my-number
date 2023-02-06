1048b944771411bc03a2cc3cd593399a3a577c06
danielthane <dthane8294@hotmail.co.uk>
2023-02-06 21:48:31 +0000

    functionality added

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 
 let compNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 console.log(compNumber);
 
 document.querySelector('.check').addEventListener('click', () => {
   let guess = Number(document.querySelector('.guess').value);
-  document.querySelector('.number').textContent = guess;
+
   // NO GUESS GIVEN
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number! ';
@@ -38,5 +39,21 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = '🎉 Correct Number! ';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = guess;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  compNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
 });
