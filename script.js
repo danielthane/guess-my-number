fd65d06ff6c0655dbaa97733dc4e252b76fe891e
danielthane <dthane8294@hotmail.co.uk>
2023-02-06 22:08:43 +0000

    final changes

--- script.js
+++ script.js
@@ -3,40 +3,34 @@
 let compNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
-console.log(compNumber);
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', () => {
   let guess = Number(document.querySelector('.guess').value);
 
   // NO GUESS GIVEN
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number! ';
+    displayMessage('⛔ No Number! ');
   }
-  // GUESS TOO HIGH
-  else if (guess > compNumber) {
+
+  //WHEN GUESS IS WRONG
+  else if (guess !== compNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '👆 Too High! ';
+      displayMessage(guess > compNumber ? '👆 Too High! ' : '👇 Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
+      displayMessage('💥 You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
-  // GUESS TOO LOW
-  else if (guess < compNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '👆 Too low! ';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-  }
+
   // WINNING GUESS
   else {
-    document.querySelector('.message').textContent = '🎉 Correct Number! ';
+    displayMessage('🎉 Correct Number! ');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = guess;
@@ -51,7 +45,7 @@ document.querySelector('.again').addEventListener('click', () => {
   compNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.score').textContent = score;
